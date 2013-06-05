6a8b9a8013b7a2f8c3a17efc61c3bfa1748f758f
s2marine <s2marine0@gmail.com>
2013-06-05 16:25:10 +0800

    fix HostsX and change to python3

--- HostsX/HostsX.py
+++ HostsX/HostsX.py
@@ -5,33 +5,25 @@
 从Hosts获取hosts文件写入系统
 '''
 
-import urllib2
 import os
 import re
-import getSource
+import requests
 import time
 
 hostsPath = r'/etc/hosts'
+url = 'http://github.com/orzTech/HostsX/raw/master/hosts'
+http_proxy  = '127.0.0.1:8087'
 
-def getUpdate():
-    print u'获取HostX数据'
-    return getSource.getSource('https://github.com/orzTech/HostsX/raw/master/hosts', proxy='127.0.0.1:8087').decode('gbk').encode('utf-8')
-    #urllib2.urlopen('http://hostsx.googlecode.com/svn/trunk/HostsX.orzhosts').read()
-
-def getUpdateList():
-    updateList = []
-    with open('updateList.txt', 'r') as sfile:
-        for line in sfile:
-            updateList.append(line.strip())
-    return updateList
+def getDate():
+    r = requests.get(url)
+    return str(r.text)
 
 def getCustom():
-    custom = u'\n### 用户自定义项\n'.encode('utf-8')
-    custom += open('custom.txt', 'r').read().encode('utf-8')
+    custom = u'\n### user custom\n'
+    custom += open('custom.txt', 'r').read()
     return custom
 
 def dataFilter(data, list):
-    print u'写入HostX数据'
     sfile = open(hostsPath, 'w')
     splitData = re.split('^(@.+)$', data, flags=re.M)
     for i in range(1, len(splitData), 2):
@@ -43,13 +35,13 @@ def dataFilter(data, list):
     sfile.close()
 
 def main():
-    if os.path.isfile(hostsPath):
-        os.remove(hostsPath)
-    data = getUpdate()
-    open('try.txt', 'w').write(data)
-    list = getUpdateList()
+    os.chdir(os.path.dirname(__file__))
+    print('获取HostX数据')
+    data = getDate()
+    #open('try.txt', 'w').write(data)
+    print(u'写入HostX数据')
     dataFilter(data, list)
-    print u'写入HostX数据完成'
+    print(u'写入HostX数据完成')
     time.sleep(2)
 
 if __name__=='__main__':
