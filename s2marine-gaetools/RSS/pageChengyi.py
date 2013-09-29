360f2185b405afe58a26931b0c39b4f3bdd7e66d
s2marine <s2marine0@gmail.com>
2013-09-29 20:05:28 +0800

    fix ConnectionError

--- s2marine-gaetools/RSS/pageChengyi.py
+++ s2marine-gaetools/RSS/pageChengyi.py
@@ -49,87 +49,95 @@ class RSSChengyi(RSSObject):
 
         waitList = []
 
-        #教务部
-        url = u'http://cyjwb.jmu.edu.cn/lists.asp?lbm=%CD%A8%D6%AA%B9%AB%B8%E6'
-        r = requests.get(url)
-        src = r.content
-        r.close()
-        soup = BeautifulSoup(src)
-        table = soup.findAll('table')[2]
-        p = re.compile(u'\d+-\d+-\d+ \d+:\d+:\d+')
-        for i in table.findAll('a')[:-3]: #去掉最后的翻页
-            title = i.text.strip()
-            link = 'http://cyjwb.jmu.edu.cn/'+i.get('href')
-            description = ''
-            guid = link
-            if guid in oldGuids:
-                continue
-            pubDateStr = p.search(i.next_sibling.next_sibling.text).group()
-            pubDate = datetime.datetime.strptime(pubDateStr, '%Y-%m-%d %H:%M:%S')
-            pubDate -= datetime.timedelta(hours=8)
-            waitList.append({
-                'title':title,
-                'link':link,
-                'description':description,
-                'guid':guid,
-                'pubDate':pubDate})
-        
+        try:
+            #教务部
+            url = u'http://cyjwb.jmu.edu.cn/lists.asp?lbm=%CD%A8%D6%AA%B9%AB%B8%E6'
+            r = requests.get(url)
+            src = r.content
+            r.close()
+            soup = BeautifulSoup(src)
+            table = soup.findAll('table')[2]
+            p = re.compile(u'\d+-\d+-\d+ \d+:\d+:\d+')
+            for i in table.findAll('a')[:-3]: #去掉最后的翻页
+                title = i.text.strip()
+                link = 'http://cyjwb.jmu.edu.cn/'+i.get('href')
+                description = ''
+                guid = link
+                if guid in oldGuids:
+                    continue
+                pubDateStr = p.search(i.next_sibling.next_sibling.text).group()
+                pubDate = datetime.datetime.strptime(pubDateStr, '%Y-%m-%d %H:%M:%S')
+                pubDate -= datetime.timedelta(hours=8)
+                waitList.append({
+                    'title':title,
+                    'link':link,
+                    'description':description,
+                    'guid':guid,
+                    'pubDate':pubDate})
+        except ConnectionError:
+            logging.info("教务部出错")
 
-        #诚毅学院
-        url = u'http://chengyi.jmu.edu.cn/NewsList.aspx?level=2&type=通知通告'
-        r = requests.get(url)
-        src = r.content
-        r.close()
-        soup = BeautifulSoup(src)
-        table = soup.find('div', attrs={'id':'div_con'}).find('table')
-        p = re.compile(u'\d+年\d+月\d+日')
-        for tr in table.findAll('tr'):
-            tds = tr.findAll('td')
-            if len(tds)!=3:
-                continue
-            title = tds[1].text.strip()
-            link = 'http://chengyi.jmu.edu.cn/'+tds[1].a.get('href')
-            description = ''
-            guid = link
-            if guid in oldGuids:
-                continue
-            pubDateStr = p.search(tds[2].text).group()
-            pubDate = datetime.datetime.strptime(pubDateStr.encode('utf-8'), '%Y年%m月%d日')
-            pubDate -= datetime.timedelta(hours=8)
-            waitList.append({
-                'title':title,
-                'link':link,
-                'description':description,
-                'guid':guid,
-                'pubDate':pubDate})
+        try:
+            #诚毅学院
+            url = u'http://chengyi.jmu.edu.cn/NewsList.aspx?level=2&type=通知通告'
+            r = requests.get(url)
+            src = r.content
+            r.close()
+            soup = BeautifulSoup(src)
+            table = soup.find('div', attrs={'id':'div_con'}).find('table')
+            p = re.compile(u'\d+年\d+月\d+日')
+            for tr in table.findAll('tr'):
+                tds = tr.findAll('td')
+                if len(tds)!=3:
+                    continue
+                title = tds[1].text.strip()
+                link = 'http://chengyi.jmu.edu.cn/'+tds[1].a.get('href')
+                description = ''
+                guid = link
+                if guid in oldGuids:
+                    continue
+                pubDateStr = p.search(tds[2].text).group()
+                pubDate = datetime.datetime.strptime(pubDateStr.encode('utf-8'), '%Y年%m月%d日')
+                pubDate -= datetime.timedelta(hours=8)
+                waitList.append({
+                    'title':title,
+                    'link':link,
+                    'description':description,
+                    'guid':guid,
+                    'pubDate':pubDate})
+        except ConnectionError:
+            logging.info("诚毅学院出错")
 
 
-        #体育教研部
-        url = u'http://cytyjys.jmu.edu.cn/Info/?id=2'
-        r = requests.get(url)
-        src = r.content
-        r.close()
-        soup = BeautifulSoup(src)
-        div = soup.find('div', attrs={'class':'m10'})
-        titles = [i.find('a') for i in div.findAll('h1', attrs={'class':'list_title'})]
-        links = [i.get('href') for i in titles]
-        titles = [i.text for i in titles]
-        pubDateStrs = [i.findAll('span')[2].text for i in div.findAll('div', attrs={'class':'list_other'})]
-        for i in range(0, len(titles)):
-            title = titles[i]
-            link = 'http://cytyjys.jmu.edu.cn/'+links[i]
-            description = ''
-            guid = link
-            if guid in oldGuids:
-                continue
-            pubDate = datetime.datetime.strptime(pubDateStrs[i], '%Y-%m-%d %H.%M.%S')
-            pubDate -= datetime.timedelta(hours=8)
-            waitList.append({
-                'title':title,
-                'link':link,
-                'description':description,
-                'guid':guid,
-                'pubDate':pubDate})
+        try:
+            #体育教研部
+            url = u'http://cytyjys.jmu.edu.cn/Info/?id=2'
+            r = requests.get(url)
+            src = r.content
+            r.close()
+            soup = BeautifulSoup(src)
+            div = soup.find('div', attrs={'class':'m10'})
+            titles = [i.find('a') for i in div.findAll('h1', attrs={'class':'list_title'})]
+            links = [i.get('href') for i in titles]
+            titles = [i.text for i in titles]
+            pubDateStrs = [i.findAll('span')[2].text for i in div.findAll('div', attrs={'class':'list_other'})]
+            for i in range(0, len(titles)):
+                title = titles[i]
+                link = 'http://cytyjys.jmu.edu.cn/'+links[i]
+                description = ''
+                guid = link
+                if guid in oldGuids:
+                    continue
+                pubDate = datetime.datetime.strptime(pubDateStrs[i], '%Y-%m-%d %H.%M.%S')
+                pubDate -= datetime.timedelta(hours=8)
+                waitList.append({
+                    'title':title,
+                    'link':link,
+                    'description':description,
+                    'guid':guid,
+                    'pubDate':pubDate})
+        except ConnectionError:
+            logging.info("体育教研部出错")
 
         oldRSSDatas += waitList
         oldRSSDatas = sorted(oldRSSDatas, key=lambda x:x['pubDate'], reverse = True)[:self.MAXItems]
